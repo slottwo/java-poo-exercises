f526bcfe1a55b5d6d29d0ab48c2138df49a44dfa
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2023-03-03 20:34:47 -0300

    Initial Commit

---
+++ src/EvenNumbersRange.java
@@ -0,0 +1,8 @@
+
+public class EvenNumbersRange {
+  public static void main(String[] args) throws Exception {
+    for (int i = 299 + 1; i > 101; i -= 2) {
+      System.out.println(i);
+    }
+  }
+}
